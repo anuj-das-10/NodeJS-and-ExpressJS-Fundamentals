d0d37c2579d55de63662a27fa505b27d11181571
Anuj Das <anonboy1434@gmail.com>
2023-02-17 14:17:02 +0530

    Updated

--- NodeJS/03-Modules.js
+++ NodeJS/03-Modules.js
@@ -7,9 +7,9 @@
 
 
 // Imports
-const names = require('./5-names');
-const sayHello = require('./4-utils');
-const data = require('./6-alternative-export-syntax')
+const names = require('./05-names');
+const sayHello = require('./04-utils');
+const data = require('./06-alternative-export-syntax')
 
 sayHello(names.anuj)
 sayHello(names.pragati)
@@ -22,4 +22,4 @@ console.log(data);
 
 
 // Imports and Run by default if function is invoked in external module
-require('./7-addValues')
+require('./07-addValues')
